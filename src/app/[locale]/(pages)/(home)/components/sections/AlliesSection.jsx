0b725abebf09016deed4c31d5829ca9e3b2a2a4b
Sebastian Morales PC-escritorio <sebasmoralesd@gmail.com>
2026-04-09 09:51:36 -0500

    Allies Section

--- src/app/[locale]/(pages)/(home)/components/sections/AlliesSection.jsx
+++ src/app/[locale]/(pages)/(home)/components/sections/AlliesSection.jsx
@@ -10,9 +10,13 @@ export default function AlliesSection() {
     const t = useTranslations('Pages.homePage.sections.allies');
     const locale = useLocale();
 
-    const groupImage = locale === 'es'
-        ? "/assets/imgs/page/group_es.png"
-        : "/assets/imgs/page/group_en.png";
+
+    // Lista de imágenes de aliados
+    const allies = [
+        { src: "/assets/imgs/page/recovery_repatriation_h.webp", alt: "Logo recovery repatriation foundation" },
+        { src: "/assets/imgs/page/flag.webp", alt: "Colombia EEUU" },
+        { src: "/assets/imgs/page/propensiones_h.webp", alt: "Logo propensiones abogados" },
+    ];
 
     const propensiones_apps = [
         {
@@ -23,11 +27,35 @@ export default function AlliesSection() {
         }
     ];
 
+    const groupImage = locale === 'es'
+        ? "/assets/imgs/page/group_es.png"
+        : "/assets/imgs/page/group_en.png";
+
     return (
         <section id="allies">
             <div className="container py-5 text-center">
                 <TitleComponent title={t('title')} aos="fade-right" />
                 <SubTitleComponent t={t} sub_title={'sub_title'} aos="fade-right" aosDelay="100" />
+
+                
+
+                <div className="row mt-4 justify-content-center align-items-center">
+                    {allies.map((ally, index) => (
+                        <div className="col-4 col-md-3 mb-4" key={index}>
+                            <div className="allies-logo-box">
+                                <Image
+                                    data-aos="zoom-in"
+                                    data-aos-delay={index * 100}
+                                    src={ally.src}
+                                    alt={ally.alt}
+                                    width={900}
+                                    height={222}
+                                    className={`img-fluid rounded shadow-sm allies-logo ${ally.src.includes("flag") ? "allies-flag" : ""}`}
+                                />
+                            </div>
+                        </div>
+                    ))}
+                </div>
 
                 <div className="row my-4 justify-content-center align-items-center">
                     <Image
@@ -39,34 +67,6 @@ export default function AlliesSection() {
                         height={222}
                         className="img-fluid rounded shadow-sm allies-logo"
                     />
-                </div>
-
-                <SubTitleComponent t={t} sub_title={'sub_title_apps'} aos="fade-right" aosDelay="100" />
-
-                <div className="row mt-4 justify-content-center">
-                    <div className="col-3 mb-4" data-aos="zoom-in" data-aos-delay={1 * 100}>
-                        <Image
-                            src="/assets/imgs/page/gea_app.webp"
-                            alt="Logo GEA"
-                            width={900}
-                            height={222}
-                            className="img-fluid rounded shadow-sm"
-                        />
-                    </div>
-                </div>
-
-                <div className="row mt-4 justify-content-center">
-                    {propensiones_apps.map((ally, index) => (
-                        <div className="col-3 mb-4" key={index} data-aos="zoom-in" data-aos-delay={index * 100}>
-                            <Image
-                                src={ally.src}
-                                alt={ally.alt}
-                                width={750}
-                                height={185}
-                                className="img-fluid rounded shadow-sm"
-                            />
-                        </div>
-                    ))}
                 </div>
             </div>
         </section>
