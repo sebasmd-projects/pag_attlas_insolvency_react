a16f9ceadfad8868fd9c50728c085b2721bf90b1
Sebastian Morales PC-escritorio <sebasmoralesd@gmail.com>
2026-05-25 14:25:26 -0500

    fix: use env api base url

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@ export const port = process.env.PORT || 3000;
 export const host = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
   : `http://localhost:${port}`;
-export const apiBaseUrl = "https://propensionesabogados.com/api/v1";
+export const apiBaseUrl = process.env.API_BASE_URL || "https://propensionesabogados.com/api/v1";
